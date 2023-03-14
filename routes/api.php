43f1ac55ea6bada0f275b375570f3a3f6f580d7f
Alexandre Silva <alexssilva@protonmail.com>
2023-03-14 22:37:55 +0000

    Added resource routes for drivers and vehicles

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\DriverController;
+use App\Http\Controllers\api\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resources([
+    'drivers' => DriverController::class,
+    'vehicles' => VehicleController::class,
+]);
