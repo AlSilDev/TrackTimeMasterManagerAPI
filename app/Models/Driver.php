87d6b5272df1ef69b0d2035340a5d5cc33b05302
Alexandre Silva <alexssilva@protonmail.com>
2023-03-14 22:20:07 +0000

    Created driver model

---
+++ app/Models/Driver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Driver extends Model
+{
+    use HasFactory, HasTimestamps;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'license_num',
+        'license_expiry',
+        'phone_num',
+        'affiliate_num'
+    ]
+}
