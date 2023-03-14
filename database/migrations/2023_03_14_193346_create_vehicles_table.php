6fd773bc22929e73cecf806e189b121434ddd3b0
Alexandre Silva <alexssilva@protonmail.com>
2023-03-14 21:30:36 +0000

    Fixed vehicles table name

--- database/migrations/2023_03_14_193346_create_vehicles_table.php
+++ database/migrations/2023_03_14_193346_create_vehicles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vehicle', function (Blueprint $table) {
+        Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('model');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vehicle');
+        Schema::dropIfExists('vehicles');
     }
 };
