1ceba2f4d2e327080e081b0e2bcdc1bfb8f313eb
Alexandre Silva <alexssilva@protonmail.com>
2023-03-14 19:28:42 +0000

    Created migrations file for drivers

---
+++ database/migrations/2023_03_14_192242_create_drivers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->integer('license_num')->unique();
+            $table->date('license_expiry');
+            $table->integer('phone_num')->unique();
+            $table->integer('affiliate_num')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
