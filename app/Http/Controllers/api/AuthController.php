351150613b0b5708c6046df9ea4ef07c8bfbbc60
Marcos Sousa <100563012+Marquinhos211@users.noreply.github.com>
2023-04-03 01:20:29 +0100

    Different key client
    Different key client

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 const PASSPORT_SERVER_URL = "http://127.0.0.1";
 
 const CLIENT_ID = 2;
-const CLIENT_SECRET = 'H1btSUsFebByPqXIapDnHsMm7fuPPZ0GX45V6DS9';
+const CLIENT_SECRET = 'tvglZFomECjwdmXfrUZ25h2QOHDNSrwNHgDAFdnP';
 
 class AuthController extends Controller
 {
