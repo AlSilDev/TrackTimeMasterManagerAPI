9b5937ef6667a8a3e8f71ce2e7005edc63297d0c
Alexandre Silva <alexssilva@protonmail.com>
2023-05-02 17:29:46 +0100

    Implemented events table migration file

---
+++ database/migrations/2023_05_02_161629_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('date_start_enrollments');
+            $table->date('date_end_enrollments');
+            $table->date('date_start_event');
+            $table->date('date_end_event');
+            $table->year('year');
+            $table->string('course_url');
+            //$table->foreignIdFor(EventCategories::class);
+            $table->integer('base_penalty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
