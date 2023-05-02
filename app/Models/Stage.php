ae4fe86e77956237c899b8ed65239f3748177c0f
Alexandre Silva <alexssilva@protonmail.com>
2023-05-02 18:49:22 +0100

    Defined stage relationships

--- app/Models/Stage.php
+++ app/Models/Stage.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Stage extends Model
 {
@@ -15,4 +16,9 @@ class Stage extends Model
         'name',
         'date_start',
     ];
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
