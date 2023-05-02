431b591bdf903df8df0108ae3017664173982137
Marcos Sousa <marcossilvasousa211@gmail.com>
2023-05-02 19:02:22 +0100

    EventCategory, Press and Regulation Models implemented

--- app/Models/EventCategory.php
+++ app/Models/EventCategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EventCategory extends Model
 {
@@ -17,7 +18,7 @@ class EventCategory extends Model
 
     protected $table="event_categories";
 
-    public function event(): hasMany
+    public function event(): HasMany
     {
         return $this->hasMany(Event::class);
     }
--- app/Models/Regulation.php
+++ app/Models/Regulation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Regulation extends Model
 {
@@ -16,7 +17,7 @@ class Regulation extends Model
         'file_url'
     ];
 
-    public function event(): belongsTo
+    public function event(): BelongsTo
     {
         return $this->belongsTo(Event::class);
     }
