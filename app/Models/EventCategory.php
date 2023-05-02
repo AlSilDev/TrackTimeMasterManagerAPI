240f38a5000447536d6fe7861effbdc8ff2e7c49
Alexandre Silva <alexssilva@protonmail.com>
2023-05-02 17:59:52 +0100

    Implemented Event Category migration and model

---
+++ app/Models/EventCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventCategory extends Model
+{
+    use HasFactory, HasTimestamps;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    protected $table="event_categories";
+}
