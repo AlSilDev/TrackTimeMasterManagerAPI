0fc874eac3da661cedc82e445662bab446068ec6
Alexandre Silva <alexssilva@protonmail.com>
2023-05-05 22:58:43 +0100

    Added soft delete support to vehicle categories

--- database/migrations/2023_05_05_213200_create_vehicle_categories_table.php
+++ database/migrations/2023_05_05_213200_create_vehicle_categories_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->foreignIdFor(VehicleClass::class, 'category_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
