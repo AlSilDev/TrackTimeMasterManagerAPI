50e7a96d70bcaf699866de32ab62aa3f6708f6f8
Marcos Sousa <marcossilvasousa211@gmail.com>
2023-05-05 22:59:36 +0100

    Update vehicle class migration
    Add attribute name and softDeletes

--- database/migrations/2023_05_05_213212_create_vehicle_classes_table.php
+++ database/migrations/2023_05_05_213212_create_vehicle_classes_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('vehicle_classes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
         });
     }
 
