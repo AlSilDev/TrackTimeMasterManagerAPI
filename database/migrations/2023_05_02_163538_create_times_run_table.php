b7ed4179a574b4a3df1ad7e21069f1ef62cbe138
Alexandre Silva <alexssilva@protonmail.com>
2023-05-06 00:27:50 +0100

    Created times run table

--- database/migrations/2023_05_02_163538_create_times_run_table.php
+++ database/migrations/2023_05_02_163538_create_times_run_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Stage;
 use App\Models\Enrollment;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -15,14 +16,22 @@ return new class extends Migration
     {
         Schema::create('times_run', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->foreignIdFor(Stage::class);
             $table->foreignIdFor(Enrollment::class);
             $table->date('start_date');
             $table->date('end_date');
+            $table->date('arrival_date');
+            $table->date('departure_date');
             $table->integer('time_secs');
-            $table->boolean('first_run_flag');
             $table->integer('time_mils');
+            $table->boolean('started');
+            $table->boolean('arrived');
+            $table->integer('penalty');
+            $table->foreignIdFor(User::class, 'penalty_updated_by');
+            $table->string('penalty_notes');
+            $table->decimal('time_points');
+            $table->decimal('run_points');
+            $table->timestamps();
         });
     }
 
