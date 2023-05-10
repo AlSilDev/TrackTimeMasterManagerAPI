f472ff689d0df1e539cae209b63890b24d773c47
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2023-05-10 16:13:15 +0100

    Implemented index method for vehicle class controller

--- app/Http/Controllers/api/VehicleClassController.php
+++ app/Http/Controllers/api/VehicleClassController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\VehicleClass;
 use Illuminate\Http\Request;
 
 class VehicleClassController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(VehicleClass::all());
+    }
 }
