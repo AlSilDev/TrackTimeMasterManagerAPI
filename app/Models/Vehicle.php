e2e3d17eebb03af69e8063d532f91dffbc12baf3
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2023-05-10 16:12:51 +0100

    Updated vehicle model relationship with category

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vehicle extends Model
@@ -31,6 +32,13 @@ class Vehicle extends Model
     public function class(): BelongsTo
     {
         return $this->belongsTo(VehicleClass::class, 'class_id');
+    }
+
+    public function category()
+    {
+        //return $this->throughClass()->has('category');
+        //return $this->hasOneThrough(VehicleCategory::class, VehicleClass::class, 'category_id', 'id', 'class_id');
+        return $this->belongsTo(VehicleClass::class, 'class_id')->join('vehicle_categories', 'vehicle_categories.id', '=', 'vehicle_classes.category_id');
     }
 
     public function history(): HasMany
