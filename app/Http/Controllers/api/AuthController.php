0303631b440121ce6c2437eb5b9d857d86358700
Alexandre Silva <alexssilva@protonmail.com>
2023-05-23 23:58:10 +0100

    Implemented video link storage request

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 const PASSPORT_SERVER_URL = "http://127.0.0.1";
 
 const CLIENT_ID = 2;
-const CLIENT_SECRET = 'cCm5pxYEwlrrayLdACdS862Irm5XTpRB3NE9X52C';
+const CLIENT_SECRET = 'SIJxjfaLv5v7vXujUqtHXy3VkOsH8VmlnB5RvaTM';
 
 class AuthController extends Controller
 {
