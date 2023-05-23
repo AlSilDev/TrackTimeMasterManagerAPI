f7dffc321043442d359337c683c35597419eaf36
Alexandre Silva <alexssilva@protonmail.com>
2023-05-24 00:28:22 +0100

    Implemented regulation controller and required methods

--- app/Http/Controllers/api/RegulationController.php
+++ app/Http/Controllers/api/RegulationController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRegulationFileRequest;
+use App\Http\Resources\RegulationResource;
+use App\Models\Event;
+use App\Models\Regulation;
+use Illuminate\Support\Facades\Storage;
 
 class RegulationController extends Controller
 {
-    //
+     /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return Regulation::where('event_id', '=', $event->id)->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRegulationFileRequest $request, Event $event)
+    {
+        //dd($request->validated());
+
+        $regulation = $request->validated();
+        $regulation['event_id'] = $event->id;
+
+        if ($request->hasFile('regulation_file')) {
+            $path = Storage::putFile('public/regulamentos', $request->file('regulation_file'));
+            $regulation['file_url'] = basename($path);
+        }
+
+        $newRegulation = Regulation::create($regulation);
+
+        return $newRegulation;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Regulation $regulation)
+    {
+        $regulation->delete();
+        return new RegulationResource($regulation);
+    }
 }
