76da58831784fd8d729970fb3013ac912a633e21
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2023-05-31 00:46:42 +0100

    Implemented "store" method in enrollment controller

--- app/Http/Controllers/api/EnrollmentController.php
+++ app/Http/Controllers/api/EnrollmentController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEnrollmentRequest;
+use App\Models\Enrollment;
 use Illuminate\Http\Request;
 
 class EnrollmentController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEnrollmentRequest $request)
+    {
+        $newEnrollment = Enrollment::create($request->validated());
+        return $newEnrollment;
+    }
 }
