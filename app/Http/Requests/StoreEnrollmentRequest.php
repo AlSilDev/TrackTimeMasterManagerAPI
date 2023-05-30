0c2cfa397b772d8d3f0f7643ca68ec721d1e5ae0
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2023-05-31 00:46:19 +0100

    Created StoreEnrollmentRequest.php

---
+++ app/Http/Requests/StoreEnrollmentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEnrollmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_driver_id' => 'required|integer|min:0',
+            'second_driver_id' => 'required|integer|min:0',
+            'vehicle_id' => 'required|integer|min:0',
+            'enrolled_by_id' => 'required|integer|min:0',
+            'event_id' => 'required|integer|min:0'
+        ];
+    }
+}
