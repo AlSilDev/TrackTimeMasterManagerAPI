bb05e78c45124e1f959510143659db04965249f8
Alexandre Silva <alexssilva@protonmail.com>
2023-06-08 01:28:11 +0100

    Made enrollment queries return history data

--- app/Http/Controllers/api/EnrollmentController.php
+++ app/Http/Controllers/api/EnrollmentController.php
@@ -14,10 +14,10 @@ class EnrollmentController extends Controller
     public function index()
     {
         return response()->json(DB::table('enrollments AS e')
-                                ->select('e.id', 'e.event_id', 'fd.name AS first_driver_name', 'sd.name AS second_driver_name', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate')
-                                ->join('drivers AS fd', 'e.first_driver_id', '=', 'fd.id')
-                                ->join('drivers AS sd', 'e.second_driver_id', '=', 'sd.id')
-                                ->join('vehicles AS v', 'e.vehicle_id', '=', 'v.id')
+                                ->select('e.id', 'e.event_id', 'e.enroll_order', 'e.run_order', 'fd.name AS first_driver_name', 'sd.name AS second_driver_name', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate')
+                                ->join('driver_history AS fd', 'e.first_driver_id', '=', 'fd.id')
+                                ->join('driver_history AS sd', 'e.second_driver_id', '=', 'sd.id')
+                                ->join('vehicle_history AS v', 'e.vehicle_id', '=', 'v.id')
                                 ->get());
     }
 
@@ -46,10 +46,10 @@ class EnrollmentController extends Controller
     public function getEventEnrollments(int $eventId)
     {
         return response()->json(DB::table('enrollments AS e')
-                                ->select('e.id', 'e.event_id', 'fd.name AS first_driver_name', 'sd.name AS second_driver_name', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate')
-                                ->join('drivers AS fd', 'e.first_driver_id', '=', 'fd.id')
-                                ->join('drivers AS sd', 'e.second_driver_id', '=', 'sd.id')
-                                ->join('vehicles AS v', 'e.vehicle_id', '=', 'v.id')
+                                ->select('e.id', 'e.event_id', 'e.enroll_order', 'e.run_order', 'fd.name AS first_driver_name', 'sd.name AS second_driver_name', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate')
+                                ->join('driver_history AS fd', 'e.first_driver_id', '=', 'fd.id')
+                                ->join('driver_history AS sd', 'e.second_driver_id', '=', 'sd.id')
+                                ->join('vehicle_history AS v', 'e.vehicle_id', '=', 'v.id')
                                 ->where('e.event_id', $eventId)
                                 ->get());
     }
