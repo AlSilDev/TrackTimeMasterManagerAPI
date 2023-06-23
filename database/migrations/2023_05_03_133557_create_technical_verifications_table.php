646ffbf4f785f5200779d6d949c966135fe786f1
Marcos Sousa <marcossilvasousa211@gmail.com>
2023-06-23 23:48:44 +0100

    Change name of VT migrations

--- database/migrations/2023_05_03_133557_create_technical_verifications_table.php
+++ database/migrations/2023_05_03_133557_create_technical_verifications_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Enrollment;
+use App\Models\Event;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,7 +16,9 @@ return new class extends Migration
     {
         Schema::create('technical_verifications', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Enrollment::class);
+            $table->foreignIdFor(Event::class, 'event_id');
+            $table->foreignIdFor(Enrollment::class, 'enrollment_id');
+            $table->boolean('enrollment_order');
             $table->boolean('verified');
             $table->string('notes');
             $table->foreignIdFor(User::class, 'verified_by');
