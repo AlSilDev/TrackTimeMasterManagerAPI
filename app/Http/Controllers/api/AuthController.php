cdd4f1293e2c3723849a2248613bd9e3251831d2
Marcos Sousa <marcossilvasousa211@gmail.com>
2023-07-03 23:15:01 +0100

    Implemented user login with only alowed logins
    User with blocked account cant login

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 const PASSPORT_SERVER_URL = "http://127.0.0.1";
 
 const CLIENT_ID = 2;
-const CLIENT_SECRET = 'nQ78dou0Dqwk9RuLwydECvNEfi98wJYWWUrRY2lT';
+const CLIENT_SECRET = 'iovynh8n9XevlUN6Nx9o6P1leJkFo6dflWyoXIga';
 
 class AuthController extends Controller
 {
@@ -25,17 +26,25 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        try {
-            request()->request->add($this->passportAuthenticationData($request->username, $request->password));
-            $request = Request::create(PASSPORT_SERVER_URL . '/oauth/token', 'POST');
-            $response = Route::dispatch($request);
-            $errorCode = $response->getStatusCode();
-            $auth_server_response = json_decode((string) $response->content(), true);
-            return response()->json($auth_server_response, $errorCode);
+        $user = User::where('email', '=', $request->username)->first();
+        if ($user->blocked != 1)
+        {
+            try {
+                request()->request->add($this->passportAuthenticationData($request->username, $request->password));
+                $request = Request::create(PASSPORT_SERVER_URL . '/oauth/token', 'POST');
+                $response = Route::dispatch($request);
+                $errorCode = $response->getStatusCode();
+                $auth_server_response = json_decode((string) $response->content(), true);
+                return response()->json($auth_server_response, $errorCode);
+            }
+            catch (\Exception $e) {
+                return response()->json('Autenticação falhada', 401);
+            }
         }
-        catch (\Exception $e) {
-            return response()->json('Authentication has failed!', 401);
+        else{
+            return response()->json('Utilizador bloqueado!', 401);
         }
+
     }
 
     public function logout(Request $request)
