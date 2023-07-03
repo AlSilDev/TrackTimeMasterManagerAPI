cde0807cd65b440f01f6592910003dd55a77dc4e
Alexandre Silva <127327458+AlSilDev@users.noreply.github.com>
2023-07-03 18:44:36 +0100

    Updated GET event participants query

--- app/Http/Controllers/api/ParticipantController.php
+++ app/Http/Controllers/api/ParticipantController.php
@@ -44,13 +44,16 @@ class ParticipantController extends Controller
     public function getAllEventParticipants(int $eventId)
     {
         return response()->json(DB::table('participants AS p')
-                                ->select('p.id', 'e.id', 'fd.name AS first_driver_name', 'e.enroll_order AS enroll_order', 'e.run_order AS run_order','sd.name AS second_driver_name', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate', 'p.can_compete')
+                                ->select('p.id', 'e.id', 'fd.name AS first_driver_name', 'fd.license_num AS first_driver_license', 'e.enroll_order AS enroll_order', 'e.run_order AS run_order','sd.name AS second_driver_name', 'sd.license_num AS second_driver_license', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate', 'vc.name AS vehicle_class', 'vcc.name AS vehicle_category', 'p.can_compete')
                                 ->join('enrollments AS e', 'e.id', 'p.enrollment_id')
-                                ->join('drivers AS fd', 'e.first_driver_id', '=', 'fd.id')
-                                ->join('drivers AS sd', 'e.second_driver_id', '=', 'sd.id')
-                                ->join('vehicles AS v', 'e.vehicle_id', '=', 'v.id')
+                                ->join('driver_history AS fd', 'e.first_driver_id', '=', 'fd.id')
+                                ->join('driver_history AS sd', 'e.second_driver_id', '=', 'sd.id')
+                                ->join('vehicle_history AS v', 'e.vehicle_id', '=', 'v.id')
+                                ->join('vehicle_classes AS vc', 'v.class_id', '=', 'vc.id')
+                                ->join('vehicle_categories AS vcc', 'vc.category_id', '=', 'vcc.id')
                                 ->join('events AS ev', 'ev.id', 'e.event_id')
                                 ->where('e.event_id', $eventId)
+                                ->orderBy('e.run_order')
                                 ->get());
     }
 
