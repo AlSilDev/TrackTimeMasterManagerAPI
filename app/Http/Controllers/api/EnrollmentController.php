5b3f2eb33331fdd726f769636690ae6eb9d783cb
Marcos Sousa <marcossilvasousa211@gmail.com>
2023-07-05 15:35:08 +0100

    Resolving bugs on enrollmentController

--- app/Http/Controllers/api/EnrollmentController.php
+++ app/Http/Controllers/api/EnrollmentController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEnrollmentRequest;
 use App\Http\Requests\UpdateEnrollmentRunOrderRequest;
 use App\Http\Resources\EnrollmentResource;
+use App\Models\AdminVerification;
 use App\Models\Enrollment;
 use App\Models\Event;
+use App\Models\Participant;
+use App\Models\TechnicalVerification;
 use App\Models\VehicleHistory;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -29,11 +32,27 @@ class EnrollmentController extends Controller
     {
         //$newEnrollment = Enrollment::create($request->validated());
         //dd($request->event_id);
+
         $enrollments_count = DB::table('enrollments AS e')
-            ->select(DB::raw('COUNT(e.id) AS count'))
+            ->select(DB::raw('COUNT(e.enroll_order) as count'))
             ->where('event_id', '=', $request->event_id)
+            ->orderBy('e.enroll_order', 'desc')
             ->get()[0]->count;
         //dd($enrollments_count);
+
+        if($enrollments_count != 0)
+        {
+            $enrollments_countAux = DB::table('enrollments AS e')
+            ->select('e.enroll_order')
+            ->where('event_id', '=', $request->event_id)
+            ->orderBy('e.enroll_order', 'desc')
+            ->get()[0]->enroll_order;
+            $enrollments_count = $enrollments_countAux;
+            //dd(++$enrollments_count);
+        }
+        //dd(++$enrollments_count);
+        //$lastmore1 = ++$enrollments_count;
+        //dd(++$enrollments_count->count);
         $validated_data = $request->validated();
         $vehicle_history_id = VehicleHistory::where('vehicle_id', '=', $validated_data['vehicle_id'])->orderBy('created_at', 'desc')->get()[0]['id'];
         //dd($vehicle_history_id);
@@ -47,8 +66,19 @@ class EnrollmentController extends Controller
 
     public function destroy(Enrollment $enrollment)
     {
+        $eventId = $enrollment->event->id;
         if (Carbon::now() <= $enrollment->event->date_end_enrollments)
+        {
             $enrollment->delete();
+            $eventEnrolls = Enrollment::where('event_id', '=', $eventId)->get();
+            foreach ($eventEnrolls as $eventEnroll) {
+                if ($eventEnroll->id > $enrollment->id){
+                    $eventEnroll->run_order -=1;
+                    $eventEnroll->save();
+                    //dd($eventEnroll->run_order);
+                }
+            }
+        }
 
         return new EnrollmentResource($enrollment);
     }
@@ -56,7 +86,7 @@ class EnrollmentController extends Controller
     public function getEventEnrollments(int $eventId)
     {
         return response()->json(DB::table('enrollments AS e')
-                                ->select('e.id', 'e.event_id', 'e.enroll_order', 'e.run_order', 'fd.name AS first_driver_name', 'fd.country AS first_driver_country', 'fd.license_num AS first_driver_license', 'sd.name AS second_driver_name', 'sd.country AS second_driver_country', 'sd.license_num AS second_driver_license', 'v.model AS vehicle_model', 'v.license_plate AS vehicle_license_plate', 'vc.name AS vehicle_class', 'vcc.name AS vehicle_category')
+                                ->select('e.id', 'e.event_id', 'e.enroll_order', 'e.run_order', 'fd.name AS first_driver_name', 'fd.id AS first_driver_id', 'fd.country AS first_driver_country', 'fd.license_num AS first_driver_license', 'sd.name AS second_driver_name', 'sd.id AS second_driver_id', 'sd.country AS second_driver_country', 'sd.license_num AS second_driver_license', 'v.model AS vehicle_model', 'v.vehicle_id AS vehicle_id', 'v.license_plate AS vehicle_license_plate', 'vc.name AS vehicle_class', 'vcc.name AS vehicle_category')
                                 ->join('driver_history AS fd', 'e.first_driver_id', '=', 'fd.id')
                                 ->join('driver_history AS sd', 'e.second_driver_id', '=', 'sd.id')
                                 ->join('vehicle_history AS v', 'e.vehicle_id', '=', 'v.id')
@@ -121,11 +151,50 @@ class EnrollmentController extends Controller
 
     public function updateRunOrder(Event $event, UpdateEnrollmentRunOrderRequest $request)
     {
-        //dd(count($request->request));
-        foreach($request->request as $enrollment)
+        //event_id = $event['id']
+        //dd($event['id']);
+        $notVerified = 0;
+        $eventEnrollsIds = DB::table('enrollments')->where('event_id', '=', $event['id'])->pluck('id');
+        $adminVerIds = DB::table('admin_verifications')->whereIn('enrollment_id', $eventEnrollsIds)->pluck('id');
+        $technicalVerIds = DB::table('technical_verifications')->whereIn('enrollment_id', $eventEnrollsIds)->pluck('id');
+        $participantsIds = DB::table('participants')->whereIn('enrollment_id', $eventEnrollsIds)->pluck('id');
+
+        if($adminVerIds->isEmpty() || $technicalVerIds->isEmpty() || $participantsIds->isEmpty())
         {
+            foreach($request->request as $enrollment)
+            {
             //dd($enrollment);
             Enrollment::where('id', '=', $enrollment['id'])->update(['run_order' => $enrollment['run_order']]);
+
+            //criar enroll em admin/ technical e participants
+            //dd($request);
+
+            //dd($enrollment['id']);
+            AdminVerification::create(['enrollment_id' => $enrollment['id'], 'verified' => $notVerified]);
+
+            TechnicalVerification::create(['enrollment_id' => $enrollment['id'], 'verified' => $notVerified]);
+
+            Participant::create(['enrollment_id' => $enrollment['id'], 'first_driver_id' => $enrollment['first_driver_id'], 'second_driver_id' => $enrollment['second_driver_id'], 'vehicle_id' => $enrollment['vehicle_id']]);
+            /*$adminVerification = new AdminVerification();
+            $adminVerification->enrollment_id = $enrollment['id'];
+            $adminVerification->save();
+
+            $technicalVerification = new TechnicalVerification();
+            $technicalVerification->enrollment_id = $enrollment['id'];
+            $technicalVerification->save();
+
+            $participant = new Participant();
+            $participant->enrollment_id = $enrollment['id'];
+            $participant->first_driver_id = $enrollment['first_driver_id'];
+            $participant->second_driver_id = $enrollment['second_driver_id'];
+            $participant->vehicle_id = $enrollment['vehicle_id'];
+            $participant->save();*/
+            }
+        }else{
+            foreach($request->request as $enrollment)
+            {
+                Enrollment::where('id', '=', $enrollment['id'])->update(['run_order' => $enrollment['run_order']]);
+            }
         }
 
         return response()->json(Enrollment::where('event_id', '=', $event['id'])->get());
